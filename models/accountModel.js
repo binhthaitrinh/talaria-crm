1f12fe44fcdd3c080df3d1e0ec478d1a435e0c07
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-05-28 10:46:59 -0700

    add transaction model

--- models/accountModel.js
+++ models/accountModel.js
@@ -1,61 +1,68 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const accountSchema = mongoose.Schema({
-  accountWebsite: {
-    type: String,
-    enun: [
-      'amazon',
-      'sephora',
-      'ebay',
-      'bestbuy',
-      'costco',
-      'walmart',
-      'others',
-    ],
-    required: [true, 'An account must come from a website'],
+const accountSchema = mongoose.Schema(
+  {
+    accountWebsite: {
+      type: String,
+      enun: [
+        'amazon',
+        'sephora',
+        'ebay',
+        'bestbuy',
+        'costco',
+        'walmart',
+        'others',
+      ],
+      required: [true, 'An account must come from a website'],
+    },
+    loginID: {
+      type: String,
+      require: [true, 'An account must have loginID'],
+    },
+    teamviewInfo: String,
+    balance: {
+      type: Number,
+      default: 0.1,
+    },
+    addedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    //   transactionHistory: [
+    //     {
+    //       transactionType: {
+    //         type: String,
+    //         enum: ['loaded', 'spent'],
+    //         requried: [true, 'A transaction must have a type: loaded or spent'],
+    //       },
+    //       amount: {
+    //         type: mongoose.Decimal128,
+    //         require: [true, 'A transaction must have amount'],
+    //       },
+    //       createdAt: {
+    //         type: Date,
+    //         default: Date.now(),
+    //       },
+    //     },
+    //   ],
+    accountType: {
+      type: String,
+      enum: ['owned', 'borrowed'],
+    },
+    notes: String,
+    status: {
+      type: String,
+      enum: ['active', 'inactive', 'disputing'],
+      default: 'active',
+    },
   },
-  loginID: {
-    type: String,
-    require: [true, 'An account must have loginID'],
-  },
-  teamviewInfo: String,
-  balance: {
-    type: Number,
-    default: 0.1,
-  },
-  addedAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  //   transactionHistory: [
-  //     {
-  //       transactionType: {
-  //         type: String,
-  //         enum: ['loaded', 'spent'],
-  //         requried: [true, 'A transaction must have a type: loaded or spent'],
-  //       },
-  //       amount: {
-  //         type: mongoose.Decimal128,
-  //         require: [true, 'A transaction must have amount'],
-  //       },
-  //       createdAt: {
-  //         type: Date,
-  //         default: Date.now(),
-  //       },
-  //     },
-  //   ],
-  accountType: {
-    type: String,
-    enum: ['owned', 'borrowed'],
-  },
-  notes: String,
-  status: {
-    type: String,
-    enum: ['active', 'inactive', 'disputing'],
-    default: 'active',
-  },
-});
+  {
+    // to include virtual properties into results
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const accountModel = mongoose.model('Account', accountSchema);
 
---
+++ models/transactionModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = mongoose.Schema({
+  transationType: {
+    type: String,
+    enum: ['inflow', 'outflow'],
+    required: [true, 'Transaction must have a type'],
+  },
+  amount: {
+    type: mongoose.Decimal128,
+    required: [true, 'Transaction must have an amount'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  accountID: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Account',
+    required: [true, 'transaction must have item ID'],
+  },
+});
+
+const transactionModel = mongoose.model('Transaction', transactionSchema);
+
+module.exports = transactionModel;

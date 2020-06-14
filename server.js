74293e9ff7e27f0e6ce67ff069f270a8ed8bdfa8
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-06-13 22:25:59 -0700

    handle mongodb error

--- server.js
+++ server.js
@@ -17,9 +17,18 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then((conn) => console.log('DB connection successful...'));
+  .then((conn) => console.log('DB connection successful...'))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 4444;
 const server = app.listen(PORT, () =>
   console.log(`Listening on port ${PORT}...`)
 );
+
+// process.on('unhandledRejection', (err) => {
+//   console.log('UNHANDLED REJECTION! Shutting down...');
+//   console.log(err.name, err.messsage);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
